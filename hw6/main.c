ae4c7f9b4ded691a829b22d3cbb96e3fb87a716a
Nathan <nathancorwin2015@u.northwestern.edu>
2016-05-13 18:57:34 -0500

    working on IMU

---
+++ hw6/main.c
@@ -0,0 +1,39 @@
+#include "pic_config.h"
+#include "i2c_master.h"
+#include "ILI9163C.h"
+#include <math.h>
+
+int main() {
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+    
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0; //Pin 12 is LED output
+    TRISBbits.TRISB4 = 1; //Pin 11 is pushbutton input      
+    LATAbits.LATA4 = 0;   //LED initilized off
+        
+    SPI1_init();
+    LCD_init();
+    
+    __builtin_enable_interrupts();
+    
+    LCD_clearScreen(WHITE);
+    
+    //Global Variables
+    signed short temp = 0,accel_x= 0, accel_y=0, accel_z=0, gyro_x=0, gyro_y=0, gyro_z=0;
+    
+    while(1){
+        
+        if(PORTBbits.RB4 == 0) {
+            LATAbits.LATA4 = !PORTAbits.RA4;
+        }
+    
+    }
+}
